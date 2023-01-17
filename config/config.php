4eafce78362fea77fdd6b52da9347bbdbfb0321e
fdlh77 <40836673+fdlh77@users.noreply.github.com>
2023-01-18 00:22:11 +0700

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define("BASE_URL", "http://localhost/IPL-TugasBesar");
+
+define("DEFAULT_CONTROLLER", "Auth");
+define("DEFAULT_METHOD", "index");
+
+define("DB_HOST", "localhost");
+define("DB_DATABASE", "db_sipa");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+session_start();
+require_once '../bootstrap/app.php';
+
+$app = new App;
